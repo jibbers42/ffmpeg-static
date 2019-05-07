d003b8c63cf31e0711c7da2c598625b4cd60570e
jibbers42 <jibbers42@gmail.com>
2019-05-07 18:42:37 -0400

    Do not download Windows or Mac ffmpeg

--- build/index.sh
+++ build/index.sh
@@ -11,17 +11,17 @@ download () {
 	curl -L -# -A 'https://github.com/eugeneware/ffmpeg-static' -o $2 $1
 }
 
-echo 'windows x64'
-echo '  downloading from ffmpeg.zeranoe.com'
-download 'https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.1.3-win64-static.zip' win32-x64.zip
-echo '  extracting'
-unzip -o -d ../bin/win32/x64 -j win32-x64.zip '**/ffmpeg.exe'
+# echo 'windows x64'
+# echo '  downloading from ffmpeg.zeranoe.com'
+# download 'https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.1.3-win64-static.zip' win32-x64.zip
+# echo '  extracting'
+# unzip -o -d ../bin/win32/x64 -j win32-x64.zip '**/ffmpeg.exe'
 
-echo 'windows ia32'
-echo '  downloading from ffmpeg.zeranoe.com'
-download 'https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-4.1.3-win32-static.zip' win32-ia32.zip
-echo '  extracting'
-unzip -o -d ../bin/win32/ia32 -j win32-ia32.zip '**/ffmpeg.exe'
+# echo 'windows ia32'
+# echo '  downloading from ffmpeg.zeranoe.com'
+# download 'https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-4.1.3-win32-static.zip' win32-ia32.zip
+# echo '  extracting'
+# unzip -o -d ../bin/win32/ia32 -j win32-ia32.zip '**/ffmpeg.exe'
 
 echo 'linux x64'
 echo '  downloading from johnvansickle.com'
@@ -35,7 +35,7 @@ download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.t
 echo '  extracting'
 $tar_exec -x -C ../bin/linux/ia32 --strip-components 1 -f linux-ia32.tar.xz --wildcards '*/ffmpeg'
 
-echo 'darwin x64'
-echo '  downloading from evermeet.cx'
-download "https://evermeet.cx/ffmpeg/ffmpeg-4.1.3.7z" darwin-x64-ffmpeg.7z
-7zr e -y -bd -o../bin/darwin/x64 darwin-x64-ffmpeg.7z >/dev/null
+# echo 'darwin x64'
+# echo '  downloading from evermeet.cx'
+# download "https://evermeet.cx/ffmpeg/ffmpeg-4.1.3.7z" darwin-x64-ffmpeg.7z
+# 7zr e -y -bd -o../bin/darwin/x64 darwin-x64-ffmpeg.7z >/dev/null
