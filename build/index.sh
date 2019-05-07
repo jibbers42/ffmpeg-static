38d33d7cb3d8e03a9a155a40ab65c614fd0c5b74
jibbers42 <jibbers42@gmail.com>
2019-05-07 16:03:20 -0400

    Update to ffmpeg 4.1.3
    - run build script on install

--- build/index.sh
+++ build/index.sh
@@ -13,29 +13,29 @@ download () {
 
 echo 'windows x64'
 echo '  downloading from ffmpeg.zeranoe.com'
-download 'https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip' win32-x64.zip
+download 'https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.1.3-win64-static.zip' win32-x64.zip
 echo '  extracting'
 unzip -o -d ../bin/win32/x64 -j win32-x64.zip '**/ffmpeg.exe'
 
 echo 'windows ia32'
 echo '  downloading from ffmpeg.zeranoe.com'
-download 'https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip' win32-ia32.zip
+download 'https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-4.1.3-win32-static.zip' win32-ia32.zip
 echo '  extracting'
 unzip -o -d ../bin/win32/ia32 -j win32-ia32.zip '**/ffmpeg.exe'
 
 echo 'linux x64'
 echo '  downloading from johnvansickle.com'
-download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz' linux-x64.tar.xz
+download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz' linux-x64.tar.xz
 echo '  extracting'
 $tar_exec -x -C ../bin/linux/x64 --strip-components 1 -f linux-x64.tar.xz --wildcards '*/ffmpeg'
 
 echo 'linux ia32'
 echo '  downloading from johnvansickle.com'
-download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz' linux-ia32.tar.xz
+download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz' linux-ia32.tar.xz
 echo '  extracting'
 $tar_exec -x -C ../bin/linux/ia32 --strip-components 1 -f linux-ia32.tar.xz --wildcards '*/ffmpeg'
 
 echo 'darwin x64'
 echo '  downloading from evermeet.cx'
-download "https://evermeet.cx/ffmpeg/getrelease" darwin-x64-ffmpeg.7z
+download "https://evermeet.cx/ffmpeg/ffmpeg-4.1.3.7z" darwin-x64-ffmpeg.7z
 7zr e -y -bd -o../bin/darwin/x64 darwin-x64-ffmpeg.7z >/dev/null
